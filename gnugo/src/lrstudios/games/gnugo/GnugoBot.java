5953920b2cedf5dd375c2c528e99bfcecf1b418f
LadaRaider <emmanuel.mathis@gmail.com>
2012-09-22 14:58:25 +0200

    Fixed indentation

--- gnugo/src/lrstudios/games/gnugo/GnugoBot.java
+++ gnugo/src/lrstudios/games/gnugo/GnugoBot.java
@@ -3,49 +3,45 @@ package lrstudios.games.gnugo;
 import lrstudios.games.ego.lib.GtpBot;
 
 
-public class GnugoBot extends GtpBot
-{
+public class GnugoBot extends GtpBot {
     private static final String BOT_NAME = "GNU Go";
     private static final String VERSION = "3.8";
 
     private static boolean init = false;
 
-	private native void initGtp(float pMemory);
-    private native String playGtp(String pInput);
-    
+    private native void initGtp(float pMemory);
 
-    static
-    {
+    private native String playGtp(String pInput);
+
+
+    static {
         System.loadLibrary("gnuGo-3.8");
     }
 
 
-    /** Creates and initializes a new GNUGo bot. It currently supports only one instance at a time. */
-    public GnugoBot()
-    {
-        if (!init)
-        {
+    /**
+     * Creates and initializes a new GNUGo bot. It currently supports only one instance at a time.
+     */
+    public GnugoBot() {
+        if (!init) {
             initGtp(8);
             init = true;
         }
     }
 
-    
+
     @Override
-    public String sendGtpCommand(String command)
-    {
+    public String sendGtpCommand(String command) {
         return playGtp(command);
     }
 
     @Override
-    public String getName()
-    {
+    public String getName() {
         return BOT_NAME;
     }
 
     @Override
-    public String getVersion()
-    {
+    public String getVersion() {
         return VERSION;
     }
 }
--- gnugo/src/lrstudios/games/gnugo/GnugoNewGameActivity.java
+++ gnugo/src/lrstudios/games/gnugo/GnugoNewGameActivity.java
@@ -3,11 +3,9 @@ package lrstudios.games.gnugo;
 import lrstudios.games.ego.lib.ui.NewGameActivity;
 
 
-public class GnugoNewGameActivity extends NewGameActivity
-{
+public class GnugoNewGameActivity extends NewGameActivity {
     @Override
-    protected Class<?> getBotClass()
-    {
+    protected Class<?> getBotClass() {
         return GnugoBot.class;
     }
 }
