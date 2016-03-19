7e981e226c5fd57ab6dbd75c7892b7a62744fe41
Dalmas <emmanuel.mathis@gmail.com>
2016-03-19 22:02:08 +0100

    Bump release : 2.01

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 adb shell pm uninstall lrstudios.games.gnugo
-adb install app-release.apk
+adb install gnugo-release.apk
 adb shell am start -n "lrstudios.games.gnugo/lrstudios.games.gnugo.GnugoNewGameActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
 pause
